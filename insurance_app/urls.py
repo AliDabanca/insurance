36f6e062c50719fdc133050a015cca86a1c9f6a8
alida <ali.dabanca064@gmail.com>
2025-10-06 16:55:30 +0300

    Fix: Added new url path

--- insurance_app/urls.py
+++ insurance_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.customer_list, name='customer_list'),
+     path('customer-list/', views.customer_list, name='customer_list'),
     path('customer-add/', views.create_customer, name='add_customer'),
     path('referanslar/', views.referanslar_view, name='referanslar'),
     path('referanslar/search/', views.referanslar_search, name='referanslar_search'),
